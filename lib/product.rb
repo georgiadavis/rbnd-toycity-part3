50846df186f0d2f4518acf9df090e5bbd0083373
Georgia Davis <georgiadavis@Georgias-MBP.wp.comcast.net>
2016-04-01 12:16:37 -0700

    updated startercode for product.rb and errors.rb

--- lib/product.rb
+++ lib/product.rb
@@ -1,5 +1,6 @@
 class Product
   @@products = []
+    attr_reader :title, :price, :stock
 
   def initialize(options={})
     @@products << self
@@ -8,4 +9,19 @@ class Product
   def self.all
     @@products
   end
+
+  def in_stock?
+    # return true or false if item is in stock or not
+  end
+
+  private
+
+  def add_to_products
+    # return error message "This toy already exists. (DuplicateProductError)"
+    @@products << self
+  end
+end
+
+class Find_by_title
+# to help you quickly return a single product based on its title
 end
